51c92d13ec0c1843ec684d347468a90cd1884b73
Armando1509 <armandogomez1509@gmail.com>
2026-02-24 12:59:04 -0600

    Actualizacion de datos, telefono y liga de linkendin

--- src/app/components/About me.js
+++ src/app/components/About me.js
@@ -36,7 +36,7 @@ const AboutMe = ({ idioma, setIdioma }) => {
 
           <div className="pr-10 flex flex-row gap-6 justify-center">
             <a
-              /* href="https://www.linkedin.com/in/jose-armando-gomez-sanchez-2b1a1424a/" */
+              href="https://www.linkedin.com/in/jose-armando-gomez-sanchez-2b1a1424a/"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-amber-400 text-white text-2xl px-10 py-3 mt-10 rounded-full hover:bg-amber-500 text-center"
